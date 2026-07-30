2f051e01dcfa90481fb4db0051770a4327a225e4
tahsintanha-blip <tahsintanha1048@gmail.com>
2026-07-30 23:13:09 +0600

    finish

---
+++ exercise1.js
@@ -0,0 +1,5 @@
+const studentName = "Tahsin Akther Tanha";
+const department = "CSE";
+let semester = 9;
+
+console.log(My name is ${studentName}. I study in the ${department} department and I am in Semester ${semester}.);
---
+++ exercise3.js
@@ -0,0 +1,4 @@
+let num = Number("19.5");
+num = num + 0.5;
+
+console.log(num);
