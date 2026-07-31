c876e05d1da7d06ce26248482ae4dc1f7e9d389b
tahsintanha-blip <tahsintanha1048@gmail.com>
2026-07-31 23:12:56 +0600

    done

---
+++ Chapter3/exercise4.js
@@ -0,0 +1,36 @@
+let month = 4;
+
+switch (month) {
+    case 1:
+    case 2:
+        console.log("Winter");
+        break;
+
+    case 3:
+    case 4:
+        console.log("Summer");
+        break;
+
+    case 5:
+    case 6:
+        console.log("Rainy");
+        break;
+
+    case 7:
+    case 8:
+        console.log("Monsoon");
+        break;
+
+    case 9:
+    case 10:
+        console.log("Autumn");
+        break;
+
+    case 11:
+    case 12:
+        console.log("Late Autumn");
+        break;
+
+    default:
+        console.log("Invalid month");
+}
