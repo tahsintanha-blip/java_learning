732a21632674ad52f9ffed02aec3468756f04f31
tahsintanha-blip <tahsintanha1048@gmail.com>
2026-08-01 23:10:28 +0600

    ok

---
+++ Chapter5/exercise2.js
@@ -0,0 +1,7 @@
+function celsiusToFahrenheit(c) {
+    return (c * 9 / 5) + 32;
+}
+
+console.log(celsiusToFahrenheit(0));   // 32
+console.log(celsiusToFahrenheit(25));  // 77
+console.log(celsiusToFahrenheit(100)); // 212
