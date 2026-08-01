64fac277ad86343efb6df3c91aa24c505a2a4c2b
tahsintanha-blip <tahsintanha1048@gmail.com>
2026-08-01 23:13:10 +0600

    ok

--- Chapter5/exercise2.js
+++ Chapter5/exercise2.js
@@ -4,4 +4,4 @@ function celsiusToFahrenheit(c) {
 
 console.log(celsiusToFahrenheit(0));   // 32
 console.log(celsiusToFahrenheit(25));  // 77
-console.log(celsiusToFahrenheit(100)); // 212
+console.log(celsiusToFahrenheit(100)); // 212gi
---
+++ Chapter5/exercise3.js
@@ -0,0 +1,9 @@
+function max3(a, b, c) {
+    if (a >= b && a >= c) {
+        return a;
+    } else if (b >= a && b >= c) {
+        return b;
+    } else {
+        return c;
+    }
+}
